013a4f7b5cf256b273a6afe5681426d43541e483
himanshu-solytics <himanshu.rathi@solytics-partners.com>
2024-09-07 13:26:07 +0530

    added predition and profile code

--- src/components/appointments/appointment-modal.tsx
+++ src/components/appointments/appointment-modal.tsx
@@ -43,10 +43,7 @@ const formValidationSchema = object({
 
 interface AppointmentModalProps {
   open: boolean;
-  handleClose?: (
-    event?: React.SyntheticEvent<any>,
-    reason?: 'backdropClick' | 'escapeKeyDown'
-  ) => void;
+  handleClose?: () => void;
   handleSubmit?: () => void;
 }
 
@@ -73,134 +70,129 @@ const AppointmentModal: React.FC<AppointmentModalProps> = ({
     { label: 'The Godfather', year: 1972 },
   ];
   return (
-    <React.Fragment>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="responsive-dialog-title"
+    <Dialog
+      open={open}
+      onClose={handleClose}
+      aria-labelledby="responsive-dialog-title"
+    >
+      <DialogTitle
+        id="responsive-dialog-title"
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+        }}
       >
-        <DialogTitle
-          id="responsive-dialog-title"
-          sx={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-          }}
-        >
-          <Typography variant="h3" component="span">
-            Book an Appointment
-          </Typography>
-          <IconButton onClick={handleClose}>
-            <CloseIcon />
-          </IconButton>
-        </DialogTitle>
-        <DialogContent>
-          <Grid container gap={4} mt={4}>
-            <Grid item xs={12}>
-              <Autocomplete
-                disablePortal
-                options={options}
-                popupIcon={<KeyboardArrowDownIcon />}
-                value={formikForm.values.doctor}
-                onChange={(_e, newValue) => {
-                  formikForm.setFieldValue('doctor', newValue);
-                }}
-                renderInput={(params) => (
-                  <TextField
-                    {...params}
-                    size="small"
-                    name="doctor"
-                    label="Select Doctor"
-                    InputLabelProps={{ shrink: true }}
-                    placeholder="Select Doctor"
-                    // {...formikForm.getFieldProps('doctor')}
-                    onBlur={formikForm.handleBlur}
-                    error={
-                      formikForm.touched.doctor?.label &&
-                      Boolean(formikForm.errors.doctor?.label)
-                    }
-                    helperText={
-                      formikForm.touched.doctor?.label &&
-                      formikForm.errors.doctor?.label
-                    }
-                  />
-                )}
+        <Typography variant="h3" component="span">
+          Book an Appointment
+        </Typography>
+        <IconButton onClick={handleClose}>
+          <CloseIcon />
+        </IconButton>
+      </DialogTitle>
+      <DialogContent>
+        <Grid container gap={4} mt={4}>
+          <Grid item xs={12}>
+            <Autocomplete
+              disablePortal
+              options={options}
+              popupIcon={<KeyboardArrowDownIcon />}
+              value={formikForm.values.doctor}
+              onChange={(_e, newValue) => {
+                formikForm.setFieldValue('doctor', newValue);
+              }}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  size="small"
+                  name="doctor"
+                  label="Select Doctor"
+                  InputLabelProps={{ shrink: true }}
+                  placeholder="Select Doctor"
+                  // {...formikForm.getFieldProps('doctor')}
+                  onBlur={formikForm.handleBlur}
+                  error={
+                    formikForm.touched.doctor?.label &&
+                    Boolean(formikForm.errors.doctor?.label)
+                  }
+                  helperText={
+                    formikForm.touched.doctor?.label &&
+                    formikForm.errors.doctor?.label
+                  }
+                />
+              )}
+            />
+          </Grid>
+          <Grid container item xs={12} spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                id="outlined-hr"
+                label="Reason for appointment"
+                placeholder="Enter issue"
+                multiline
+                {...formikForm.getFieldProps('issue')}
+                error={
+                  formikForm.touched.issue && Boolean(formikForm.errors.issue)
+                }
+                helperText={formikForm.touched.issue && formikForm.errors.issue}
               />
             </Grid>
-            <Grid container item xs={12} spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  id="outlined-hr"
-                  label="Reason for appointment"
-                  placeholder="Enter issue"
-                  multiline
-                  {...formikForm.getFieldProps('issue')}
-                  error={
-                    formikForm.touched.issue && Boolean(formikForm.errors.issue)
-                  }
-                  helperText={
-                    formikForm.touched.issue && formikForm.errors.issue
-                  }
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  id="outlined-comment"
-                  label="Additional comments/notes"
-                  placeholder="Enter comment"
-                  multiline
-                  {...formikForm.getFieldProps('comment')}
-                  error={
-                    formikForm.touched.comment &&
-                    Boolean(formikForm.errors.comment)
-                  }
-                  helperText={
-                    formikForm.touched.comment && formikForm.errors.comment
-                  }
-                />
-              </Grid>
-            </Grid>
-
-            <Grid item xs={12}>
-              <MuiDateTimePicker
-                label="Select Appointment Date"
-                disablePast
-                value={formikForm.values.dateTime}
-                onChange={(newValue: any) => {
-                  formikForm.setFieldValue('dateTime', newValue);
-                }}
-                slots={{
-                  textField: (params: any) => (
-                    <TextField
-                      {...params}
-                      error={
-                        formikForm.touched.dateTime &&
-                        Boolean(formikForm.errors.dateTime)
-                      }
-                      helperText={
-                        formikForm.touched.dateTime &&
-                        formikForm.errors.dateTime
-                      }
-                    />
-                  ),
-                }}
+            <Grid item xs={12} sm={6}>
+              <TextField
+                id="outlined-comment"
+                label="Additional comments/notes"
+                placeholder="Enter comment"
+                multiline
+                {...formikForm.getFieldProps('comment')}
+                error={
+                  formikForm.touched.comment &&
+                  Boolean(formikForm.errors.comment)
+                }
+                helperText={
+                  formikForm.touched.comment && formikForm.errors.comment
+                }
               />
             </Grid>
           </Grid>
-        </DialogContent>
-        <DialogActions sx={{ p: 3 }}>
-          <Button
-            onClick={() => formikForm.handleSubmit()}
-            fullWidth
-            variant="contained"
-            autoFocus
-            type="submit"
-          >
-            Submit
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </React.Fragment>
+
+          <Grid item xs={12}>
+            <MuiDateTimePicker
+              label="Select Appointment Date"
+              disablePast
+              value={formikForm.values.dateTime}
+              onChange={(newValue: any) => {
+                formikForm.setFieldValue('dateTime', newValue);
+              }}
+              slots={{
+                textField: (params: any) => (
+                  <TextField
+                    {...params}
+                    error={
+                      formikForm.touched.dateTime &&
+                      Boolean(formikForm.errors.dateTime)
+                    }
+                    helperText={
+                      formikForm.touched.dateTime && formikForm.errors.dateTime
+                    }
+                  />
+                ),
+              }}
+            />
+          </Grid>
+        </Grid>
+      </DialogContent>
+      <DialogActions sx={{ p: 3 }}>
+        <Button
+          onClick={() => formikForm.handleSubmit()}
+          fullWidth
+          variant="contained"
+          autoFocus
+          type="submit"
+        >
+          Submit
+        </Button>
+      </DialogActions>
+    </Dialog>
   );
 };
 
